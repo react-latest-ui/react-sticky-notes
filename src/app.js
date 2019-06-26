4e5fbc0803474e503396e13f362ecea5d20a2797
7mmsingh31246 <mmsingh31246@gmail.com>
2019-06-26 17:56:22 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import DeleteOutlined from '@material-ui/icons/DeleteOutlined';
+import AddOutlined from '@material-ui/icons/AddOutlined';
+import MoreHorizOutlined from '@material-ui/icons/MoreHorizOutlined';
 import ReactStickyNotes from './components/react-sticky-notes';
-ReactDOM.render(<ReactStickyNotes width={'800px'} height={'400px'} />, document.getElementById("root"));
+const icons = { add:AddOutlined,menu: MoreHorizOutlined, trash: DeleteOutlined };
+ReactDOM.render(<ReactStickyNotes width={'800px'} height={'400px'} icons={icons} />, document.getElementById("root"));
