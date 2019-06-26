fc1dbd64bb992e7010faabd8a3394e1dd919c19c
7mmsingh31246 <mmsingh31246@gmail.com>
2019-06-26 13:42:44 +0530

    Update note-header.js

--- src/components/react-sticky-notes/note-header.js
+++ src/components/react-sticky-notes/note-header.js
@@ -1,17 +1,15 @@
-import DeleteOutlined from '@material-ui/icons/DeleteOutlined';
-import AddOutlined from '@material-ui/icons/AddOutlined';
-import MoreHorizOutlined from '@material-ui/icons/MoreHorizOutlined';
 import { h } from './utils';
 import './note-header.scss';
-function NoteHeader({ index, prefix, addItem, deleteItem, setToggle, position, count, title, targetRef }) {
+function NoteHeader({ index, prefix, addItem, deleteItem, setToggle, position, count, title, targetRef, icons }) {
+    console.log(icons)
     return h('div',{
         className:`${prefix}--header`
     },[
         h('button',{
             key: 'note-header-button-1',
             onClick:()=>addItem(index, {position})
-        }, 'add'
-            //h(AddOutlined) 
+        }, 
+            h(icons.add)
         ),
         h('span',{
             key: 'note-header-button-2',
@@ -22,16 +20,14 @@ function NoteHeader({ index, prefix, addItem, deleteItem, setToggle, position, c
             key: 'note-header-button-3',
             onClick:()=>setToggle(index+1)
         }, 
-        'menu'
-        //h(MoreHorizOutlined) 
+            h(icons.menu) 
         ),
         h('button',{
             key: 'note-header-button-4',
             disabled: count===1?true:false,
             onClick:()=>deleteItem(index)
         }, 
-        'delete'
-        //h(DeleteOutlined) 
+            h(icons.delete)
         ),
     ])
 }
