f6758895a6a565ed31e6f1c6916b03d881322ae7
7mmsingh31246 <mmsingh31246@gmail.com>
2019-06-26 17:40:11 +0530

    Create icons.js

---
+++ src/components/react-sticky-notes/utils/icons.js
@@ -0,0 +1,4 @@
+import { h } from './h';
+export const iconAdd = () => h('span',{className: 's-icon s-icon--add'});
+export const iconTrash = () => h('span',{className: 's-icon s-icon--trash'});
+export const iconMenu = () => h('span',{className: 's-icon s-icon--menu'});
