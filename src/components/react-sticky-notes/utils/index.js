7d298853549d1f859f0f800f69fcf053dcb9cf0e
7mmsingh31246 <mmsingh31246@gmail.com>
2019-06-26 17:43:54 +0530

    Update index.js

--- src/components/react-sticky-notes/utils/index.js
+++ src/components/react-sticky-notes/utils/index.js
@@ -2,9 +2,13 @@ import { h } from './h';
 import colorCodes from './color-codes';
 import { nlToBr } from './nl-to-br';
 import { getNotes } from './get-notes';
+import { iconAdd, iconMenu, iconTrash } from './icons';
 export {
     h,
     colorCodes,
     nlToBr,
-    getNotes
+    getNotes,
+    iconAdd, 
+    iconMenu, 
+    iconTrash
 }
