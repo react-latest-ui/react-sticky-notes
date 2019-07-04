06b1b546447d2e93b56b245df94d77d87b17e8f3
7mmsingh31246 <mmsingh31246@gmail.com>
2019-07-04 13:30:13 +0530

    Update note-menu.js

--- src/components/react-sticky-notes/partials/note-menu.js
+++ src/components/react-sticky-notes/partials/note-menu.js
@@ -1,12 +1,12 @@
 import { h, getElementStyle } from './../utils';
-function NoteMenu({ index, prefix, color, colorCodes, setColor }) {
+function NoteMenu({ id, index, prefix, color, colorCodes, setColor }) {
     return h('div', {
         className: `${prefix}--colors`,
         style: getElementStyle('note-menu')
     }, 
         colorCodes.map((colorCode) =>  h('button', {
             key: colorCode,
-            onClick: () => setColor(index, colorCode),
+            onClick: () => setColor(index, {id, colorCode}),
             className: `${prefix}--colors__color ${color === colorCode ? prefix + '--colors__color--selected' : ''}`,
             style: getElementStyle('note-color-selector', {colorCode})
         }, colorCode )
