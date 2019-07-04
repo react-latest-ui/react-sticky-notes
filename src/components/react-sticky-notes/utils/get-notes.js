6b927c010d9a396de0b245e372e0116497e120f7
7mmsingh31246 <mmsingh31246@gmail.com>
2019-07-04 13:20:14 +0530

    Update get-notes.js

--- src/components/react-sticky-notes/utils/get-notes.js
+++ src/components/react-sticky-notes/utils/get-notes.js
@@ -1,6 +1,8 @@
+import getUUID from './get-uuid';
 export const getNotes = (colorCodes, notes ) => {
 	const _notes = localStorage.getItem('react-sticky-notes') ? JSON.parse(localStorage.getItem('react-sticky-notes')) : notes?notes:[
 		{
+			id: getUUID(),
 			text: '',
 			position: { x: 0, y: 0 },
 			color: colorCodes[Math.floor(Math.random() * colorCodes.length)],
