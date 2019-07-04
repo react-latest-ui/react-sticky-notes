659bbfcb148dcd4af8eb0bb4a390d46e06ee71aa
7mmsingh31246 <mmsingh31246@gmail.com>
2019-07-04 13:37:48 +0530

    Update navbar-item.js

--- src/components/react-sticky-notes/navbar/navbar-item.js
+++ src/components/react-sticky-notes/navbar/navbar-item.js
@@ -1,5 +1,5 @@
 import { h } from './../utils';
-function NavBarItem({prefix, icons,index, color, text, selectItem, deleteItem}){
+function NavBarItem({id, prefix, icons,index, color, text, selectItem, deleteItem}){
     return h('span',{
         className:`${prefix}--navbar__item`,
         style:{
@@ -10,13 +10,13 @@ function NavBarItem({prefix, icons,index, color, text, selectItem, deleteItem}){
             key: `navbar-item__${index}--select`,
             className:`${prefix}--navbar__item--select`,
             type: 'button',
-            onClick: ()=>selectItem(index, {selected:true})
+            onClick: ()=>selectItem(index, {id, selected:true})
         }, text?text:'...'),
         h('button',{
             key: `navbar-item__${index}--delete`,
             type: 'button',
             className:`${prefix}--navbar__item--delete`,
-            onClick: ()=>deleteItem(index, {selected:true}),
+            onClick: ()=>deleteItem(index, {id, selected:true}),
         }, 'x'),
     ]);
 }
