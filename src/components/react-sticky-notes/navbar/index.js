f186abf40ddf44064e30763a5dd563624d2bda1d
7mmsingh31246 <mmsingh31246@gmail.com>
2019-07-04 13:53:35 +0530

    Update index.js

--- src/components/react-sticky-notes/navbar/index.js
+++ src/components/react-sticky-notes/navbar/index.js
@@ -1,14 +1,13 @@
 import { h } from './../utils';
 import NavBarItem from './navbar-item';
 import NavBarItemAdd from './navbar-item-add';
-function NavBar({prefix, items, addItem, selectItem, deleteItem, icons, navbar}){
+function NavBar({prefix, items, addItem, selectItem, deleteItem, icons}){
     return h('div',{
         className:`${prefix}--navbar`,
     },[
-        navbar&&items?items.map((item, index)=>
+        items?items.map((item, index)=>
             h(NavBarItem,{
                 key: `navbar-item__${index}`,
-                navbar,
                 index,
                 prefix,
                 icons,
