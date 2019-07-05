57f27deb727b88e8fd3d1bb05dcaafa88ca8cdea
7mmsingh31246 <mmsingh31246@gmail.com>
2019-07-05 12:11:34 +0530

    Update navbar-item-add.js

--- src/components/react-sticky-notes/navbar/navbar-item-add.js
+++ src/components/react-sticky-notes/navbar/navbar-item-add.js
@@ -1,11 +1,11 @@
 
 import { h } from './../utils';
-function NavBarItemAdd({prefix, icons, addItem}){
+function NavBarItemAdd({prefix, icons, callbacks}){
     return h('button',{
         key: `${prefix}--navbar__item--add`,
         type: 'button',
         className:`${prefix}--navbar__item--add`,
-        onClick: ()=>addItem(),
+        onClick: (e)=>callbacks.addItem(e),
     }, icons.add);
 }
 export default NavBarItemAdd;
