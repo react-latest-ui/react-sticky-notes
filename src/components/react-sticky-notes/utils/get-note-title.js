75f10fb331bcd5a093cd9dfcc5cad549d1ba8aed
7mmsingh31246 <mmsingh31246@gmail.com>
2019-08-05 20:33:08 +0530

    Update get-note-title.js

--- src/components/react-sticky-notes/utils/get-note-title.js
+++ src/components/react-sticky-notes/utils/get-note-title.js
@@ -1,4 +1,4 @@
-export const getNoteTitle = ({title, text, limit = 10}) => {
+export const getNoteTitle = ({ title, text }, limit = 10 ) => {
 	const str = title?title:String(text).substr(0, limit);
 	return str.substr(0, 1).toUpperCase() + str.substr(1, str.length);
 }
