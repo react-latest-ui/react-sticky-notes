1faf186821e3a449044b14fa191d26fa977ba2d8
7mmsingh31246 <mmsingh31246@gmail.com>
2019-08-06 18:31:47 +0530

    Update upload-modal.js

--- src/components/react-sticky-notes/modals/upload-modal.js
+++ src/components/react-sticky-notes/modals/upload-modal.js
@@ -15,7 +15,6 @@ export class UploadModal extends Component{
     uploadFile = (e) => {
         const file = e.target.files[0];
         if(file){
-            console.log();
             if(file.type==='application/json'){
                 var reader = new FileReader();
                 reader.onload = (readerEvent) => {
