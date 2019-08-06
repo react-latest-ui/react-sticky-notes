bdffff9389f1bc23b506d5b2727dcf00c18b4a7e
7mmsingh31246 <mmsingh31246@gmail.com>
2019-08-06 18:27:37 +0530

    Update index.js

--- src/components/react-sticky-notes/navbar/index.js
+++ src/components/react-sticky-notes/navbar/index.js
@@ -1,6 +1,6 @@
 import { h, getElementStyle } from './../utils';
 import NoteHeader from './../partials/note-header';
-import { ButtonAdd, ButtonTitle, ButtonMenu, ButtonImport, ButtonTrash, ButtonPageView } from './../buttons';
+import { ButtonAdd, ButtonTitle, ButtonMenu, ButtonUpload, ButtonTrash, ButtonPageView } from './../buttons';
 function NavBar({viewSize, prefix, items, callbacks, icons}){
     const buttons = [ ButtonTitle, ButtonTrash];
     if(viewSize==='pageview'||viewSize==='fullscreen'){
@@ -35,7 +35,7 @@ function NavBar({viewSize, prefix, items, callbacks, icons}){
                 viewSize: viewSize,
                 icons,
                 callbacks,
-                buttons: [ButtonAdd, ButtonPageView, ButtonImport, ButtonTrash]
+                buttons: [ButtonAdd, ButtonPageView, ButtonUpload, ButtonTrash]
             })
         )
     ]);
