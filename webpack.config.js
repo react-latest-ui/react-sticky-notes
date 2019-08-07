86a267966576066b48f06502d05768f031d2ffff
7mmsingh31246 <mmsingh31246@gmail.com>
2019-08-07 19:23:01 +0530

    deploy to gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'index.js'
