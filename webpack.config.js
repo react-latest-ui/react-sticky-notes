45ea7d64256cc866023761a73582a6d0a9024ee2
7mmsingh31246 <mmsingh31246@gmail.com>
2019-08-07 19:31:52 +0530

    deploy to gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'index.js'
+        filename: 'main.js'
     },
     module: {
         rules: [
